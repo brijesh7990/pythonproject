65978915573912c656d0e94c6967354b639f9a4f
brijesh <brijeshubuntulaptop@gmail.com>
2025-01-06 23:03:20 +0530

    django boiler plate

--- manage.py
+++ manage.py
@@ -6,7 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
+    environment = os.getenv("DJANGO_ENV", "dev")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", f"project.settings.{environment}")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
